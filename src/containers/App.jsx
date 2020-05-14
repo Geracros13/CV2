d22167a6b39a391dbb7586285319d42aef27e5ef
Gerardo Quiñonez <gerardo01302011@gmail.com>
2020-05-14 17:17:12 -0600

    Add Loader component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
-import styled, { keyframes } from "styled-components";
+import Loader from "../styled/Loader";
 import Main from "../components/Main";
 import Sidebar from "../components/Sidebar";
 import Info from "../components/Info";
@@ -20,45 +20,11 @@ body{
 }
 `;
 
-const spin = keyframes`
-0% { transform: rotate(0deg) }
-100% { transform: rotate(360deg) }
-`;
-
-const LoaderContainer = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	padding: 75px;
-	margin: 200px;
-`;
-
-const LoaderMobile = styled.div`
-	@media only screen and (max-width: 767px) {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-`;
-
-const Loader = styled.div`
-	border: 5px solid #101010;
-	border-radius: 50%;
-	border-top: 5px solid #ff1500;
-	width: 50px;
-	height: 50px;
-	animation: ${spin} 2s linear infinite;
-`;
-
 const App = () => {
 	const data = useGetData();
 
 	return data.length === 0 ? (
-		<LoaderMobile>
-			<LoaderContainer>
-				<Loader></Loader>
-			</LoaderContainer>
-		</LoaderMobile>
+		<Loader />
 	) : (
 		<Main>
 			<GlobalStyle />
